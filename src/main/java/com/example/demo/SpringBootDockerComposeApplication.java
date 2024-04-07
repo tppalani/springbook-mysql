478183341fcafc3668e5f1c41a99e771c3ec7e50
unknown <you@example.com>
2024-04-07 20:43:15 +0530

    first

---
+++ src/main/java/com/example/demo/SpringBootDockerComposeApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerComposeApplication.class, args);
+	}
+}
